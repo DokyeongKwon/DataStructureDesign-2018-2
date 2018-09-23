938e1f56d0a1f2949d6af15d99428aeb35e3e12f
권도경 <dkdk6638@gmail.com>
2018-09-23 16:25:10 +0900

    App_Development_Ver1.0
    Activity작성

---
+++ DDARAWA/DDARAWAAPP/app/src/main/java/com/example/dkdk6/ddarawa/CategoryActivity.java
@@ -0,0 +1,16 @@
+package com.example.dkdk6.ddarawa;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dkdk6 on 2018-09-23.
+ */
+
+public class CategoryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
